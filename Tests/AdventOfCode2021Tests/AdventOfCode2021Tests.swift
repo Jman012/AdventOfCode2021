28ebf5a5a76393bf20bea260aa9bbc853c0afffc
James Linnell <james.n.linnell@gmail.com>
2021-12-01 20:37:37 -0800

    Day 02 Preparation

--- Tests/AdventOfCode2021Tests/AdventOfCode2021Tests.swift
+++ Tests/AdventOfCode2021Tests/AdventOfCode2021Tests.swift
@@ -2,16 +2,34 @@ import XCTest
 @testable import AdventOfCode2021
 
 final class Day01Tests: XCTestCase {
-	func testDay01Part1Example() throws {
-		XCTAssertEqual(Day01().solvePart1(input: Inputs.Day01.inputExample), "7")
+	var day: Challenge { Day01() }
+	func testPart1Example() throws {
+		XCTAssertEqual(day.solvePart1(input: Inputs.Day01.inputExample), "7")
 	}
-	func testDay01Part1Real() throws {
-		XCTAssertEqual(Day01().solvePart1(input: Inputs.Day01.inputMain), "1616")
+	func testPart1Real() throws {
+		XCTAssertEqual(day.solvePart1(input: Inputs.Day01.inputMain), "1616")
 	}
-	func testDay01Part2Example() throws {
-		XCTAssertEqual(Day01().solvePart2(input: Inputs.Day01.inputExample), "5")
+	func testPart2Example() throws {
+		XCTAssertEqual(day.solvePart2(input: Inputs.Day01.inputExample), "5")
 	}
-	func testDay01Part2Real() throws {
-		XCTAssertEqual(Day01().solvePart2(input: Inputs.Day01.inputMain), "1645")
+	func testPart2Real() throws {
+		XCTAssertEqual(day.solvePart2(input: Inputs.Day01.inputMain), "1645")
 	}
 }
+
+final class Day02Tests: XCTestCase {
+	var day: Challenge { Day02() }
+	func testPart1Example() throws {
+		XCTAssertEqual(day.solvePart1(input: Inputs.Day01.inputExample), "7")
+	}
+	func testPart1Real() throws {
+		XCTAssertEqual(day.solvePart1(input: Inputs.Day01.inputMain), "1616")
+	}
+	func testPart2Example() throws {
+		XCTAssertEqual(day.solvePart2(input: Inputs.Day01.inputExample), "5")
+	}
+	func testPart2Real() throws {
+		XCTAssertEqual(day.solvePart2(input: Inputs.Day01.inputMain), "1645")
+	}
+}
+
