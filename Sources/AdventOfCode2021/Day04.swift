3bcffb32ece94db50d7becdc40e600180c8ed218
James Linnell <james.n.linnell@gmail.com>
2021-12-03 21:41:09 -0800

    Day 04

--- Sources/AdventOfCode2021/Day04.swift
+++ Sources/AdventOfCode2021/Day04.swift
@@ -2,19 +2,120 @@ import Foundation
 
 struct Day04: Challenge {
 	
+	class Cell {
+		let value: Int
+		var drawn = false
+		
+		init(value: Int) {
+			self.value = value
+		}
+	}
+	
+	class Board {
+		let rows: [[Cell]]
+		var isComplete = false
+		
+		init(input: String) {
+			let rawRows = input.split(separator: "\n")
+			var rows = [[Cell]]()
+			for row in rawRows {
+				let cols = row.split(separator: " ")
+					.filter({ $0 != "" })
+					.map({ Cell(value: Int($0)!) })
+				rows.append(cols)
+			}
+			self.rows = rows
+		}
+		
+		func mark(draw: Int) {
+			for row in rows {
+				for cell in row {
+					if draw == cell.value {
+						cell.drawn = true
+					}
+				}
+			}
+		}
+		
+		func isCompleteScore() -> Int {
+			for row in rows {
+				if row.allSatisfy({ $0.drawn }) {
+					return rows
+						.flatMap({ $0 })
+						.filter({ $0.drawn == false })
+						.reduce(0, { $0 + $1.value })
+				}
+			}
+			
+			for colIdx in 0..<rows.first!.count {
+				let column = rows.map({ $0[colIdx] })
+				if column.allSatisfy({ $0.drawn }) {
+					return rows
+						.flatMap({ $0 })
+						.filter({ $0.drawn == false })
+						.reduce(0, { $0 + $1.value })
+				}
+			}
+			
+			return 0
+		}
+	}
+	
 	func solvePart1(input: String) -> String {
-		let lines = parse(input: input)
+		let (draws, boards) = parse(input: input)
+		for draw in draws {
+//			print("Draw: \(draw)")
+			for (index, board) in boards.enumerated() {
+//				print("Board #\(index)")
+				board.mark(draw: draw)
+				
+				let complete = board.isCompleteScore()
+				if complete > 0 {
+//					print("Board #\(index) on Draw \(draw) completed")
+					return "\(complete * draw)"
+				}
+			}
+		}
 		return ""
 	}
 	
 	func solvePart2(input: String) -> String {
-		var lines = parse(input: input)
+		var (draws, boards) = parse(input: input)
+		for draw in draws {
+//			print("Draw: \(draw)")
+			for (index, board) in boards.enumerated() {
+//				print("Board #\(index)")
+				
+				if !board.isComplete {
+					board.mark(draw: draw)
+					let complete = board.isCompleteScore()
+					if complete > 0 {
+//						print("Board #\(index) on Draw \(draw) completed")
+						board.isComplete = true
+	//					return "\(complete * draw)"
+					}
+				}
+				
+				if boards.filter({ !$0.isComplete }).count == 0 {
+					// This was the last board to complete
+					return "\(board.isCompleteScore() * draw)"
+				}
+			}
+		}
 		return ""
 	}
 	
-	func parse(input: String) -> [String] {
-		return input
-			.split(separator: "\n")
-			.map({ String($0) })
+	func parse(input: String) -> ([Int], [Board]) {
+		let draws = input[input.startIndex..<input.firstIndex(of: "\n")!]
+			.split(separator: ",")
+			.map({ Int($0)! })
+		
+		let sBoards = input[input.firstIndex(of: "\n")!..<input.endIndex]
+		let boards = sBoards
+			.components(separatedBy: "\n\n")
+			.filter({ $0 != "" })
+			.map({ Board(input: $0) })
+		
+		return (draws, boards)
 	}
 }
