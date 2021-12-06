22252498b41198575c10366249de77a6a79a1be0
James Linnell <james.n.linnell@gmail.com>
2021-12-05 21:21:10 -0800

    Day 06

--- Sources/AdventOfCode2021/Day06.swift
+++ Sources/AdventOfCode2021/Day06.swift
@@ -2,19 +2,82 @@ import Foundation
 
 struct Day06: Challenge {
 	
+	class FishGroup {
+		var age: Int
+		var count: Int
+		
+		var description: String {
+			"\(age): \(count)"
+		}
+		
+		init(age: Int, count: Int) {
+			self.age = age
+			self.count = count
+		}
+	}
+	
 	func solvePart1(input: String) -> String {
-		let lines = parse(input: input)
-		return ""
+		return solve(input: input, days: 80)
 	}
 	
 	func solvePart2(input: String) -> String {
-		let lines = parse(input: input)
-		return ""
+		return solve(input: input, days: 256)
+	}
+	
+	func solve(input: String, days: Int) -> String {
+		let fishAges = parse(input: input)
+		
+		var fishAgeGroups: [FishGroup] = []
+		for fishAge in fishAges {
+			if let group = fishAgeGroups.first(where: { $0.age == fishAge }) {
+				group.count += 1
+			} else {
+				fishAgeGroups.append(FishGroup(age: fishAge, count: 1))
+			}
+		}
+		
+//		print(fishAgeGroups.map({ $0.description }))
+		
+		for _ in 1...days {
+			
+			var newFish = 0
+			for group in fishAgeGroups {
+				group.age -= 1
+				if group.age < 0 {
+					group.age = 6
+					newFish += group.count
+				}
+			}
+			
+			if newFish > 0 {
+				if let newGroup = fishAgeGroups.first(where: { $0.age == 8 }) {
+					newGroup.count += newFish
+				} else {
+					fishAgeGroups.append(FishGroup(age: 8, count: newFish))
+				}
+			}
+			
+//			print("\(i): \(fishAgeGroups.map({ $0.description }))")
+			
+//			var newFish = 0
+//			fishAges = fishAges.map({ fish in
+//				var newAge = fish
+//				newAge -= 1
+//				if newAge < 0 {
+//					newAge = 6
+//					newFish += 1
+//				}
+//				return newAge
+//			})
+//			fishAges.append(contentsOf: [Int](repeating: 8, count: newFish))
+		}
+		
+		return "\(fishAgeGroups.reduce(0, { $0 + $1.count }))"
 	}
 	
 	func parse(input: String) -> [Int] {
 		return input
-			.split(separator: "\n")
+			.split(separator: ",")
 			.map({
 				Int($0)!
 			})
--- Tests/AdventOfCode2021Tests/AdventOfCode2021Tests.swift
+++ Tests/AdventOfCode2021Tests/AdventOfCode2021Tests.swift
@@ -126,21 +126,21 @@ final class Day06Tests: XCTestCase {
 	var day: Challenge { Day06() }
 	var input: Input { Inputs.Day06() }
 	func testPart1Example() throws {
-		XCTAssertEqual(day.solvePart1(input: input.inputExample1), "")
+		XCTAssertEqual(day.solvePart1(input: input.inputExample1), "5934")
 	}
 	func testPart1Real() throws {
-		print(day.solvePart1(input: input.inputMain1))
-//		measure(metrics: metrics, block: {
-//			XCTAssertEqual(day.solvePart1(input: input.inputMain1), "")
-//		})
+//		print(day.solvePart1(input: input.inputMain1))
+		measure(metrics: metrics, block: {
+			XCTAssertEqual(day.solvePart1(input: input.inputMain1), "390923")
+		})
 	}
 	func testPart2Example() throws {
-		XCTAssertEqual(day.solvePart2(input: input.inputExample2), "")
+		XCTAssertEqual(day.solvePart2(input: input.inputExample2), "26984457539")
 	}
 	func testPart2Real() throws {
-		print(day.solvePart2(input: input.inputMain2))
-//		measure(metrics: metrics, block: {
-//			XCTAssertEqual(day.solvePart2(input: input.inputMain2), "")
-//		})
+//		print(day.solvePart2(input: input.inputMain2))
+		measure(metrics: metrics, block: {
+			XCTAssertEqual(day.solvePart2(input: input.inputMain2), "1749945484935")
+		})
 	}
 }
