d44736d86b35a6980a0480b5d79e6ebc390a988c
James Linnell <james.n.linnell@gmail.com>
2021-12-11 23:02:09 -0800

    Day 12 optimize: parallel

--- Sources/AdventOfCode2021/Day12.swift
+++ Sources/AdventOfCode2021/Day12.swift
@@ -37,14 +37,51 @@ public struct Day12: Challenge {
 	}
 	
 	public func solvePart2(input: String) -> String {
+		// Original single-threaded solution
+//		let data = parse(input: input)
+//
+//		let allPaths = data
+//			.keys
+//			.filter({ $0 != "start" && $0 != "end" && $0 == $0.lowercased() })
+//			.map({ findNumPaths(data: data, node: "start", visited: ["start"], singleSmallCave: $0) })
+//			.flatMap({ $0 })
+//		let allPathsUnique = Set(allPaths)
+////		print(allPathsUnique)
+//		return "\(allPathsUnique.count)"
+		
 		let data = parse(input: input)
 		
-		let allPaths = data
+		let allSmallCaves = data
 			.keys
-			.filter({ $0 != "start" && $0 != "end" })
-			.map({ findNumPaths(data: data, node: "start", visited: ["start"], singleSmallCave: $0) })
-			.flatMap({ $0 })
-		let allPathsUnique = Set(allPaths)
+			.filter({ $0 != "start" && $0 != "end" && $0 == $0.lowercased() })
+		var queues: [DispatchQueue] = []
+		var allPathsUnique: Set<[String]> = []
+		let lock1 = NSLock()
+		let lock2 = NSLock()
+		lock2.lock()
+		var num = allSmallCaves.count
+		for (index, cave) in allSmallCaves.enumerated() {
+			let queue = DispatchQueue(label: "cave-\(index)", qos: .userInteractive, attributes: .concurrent)
+			queues.append(queue)
+			queue.async {
+//				print("\(index): async and find")
+				let newPaths = findNumPaths(data: data, node: "start", visited: ["start"], singleSmallCave: cave)
+				lock1.lock()
+//				print("\(index): locked")
+				allPathsUnique.formUnion(newPaths)
+				num -= 1
+//				print("\(index): new num = \(num)")
+				if num == 0 {
+//					print("\(index): unlock lock2")
+					lock2.unlock()
+				}
+				lock1.unlock()
+			}
+		}
+//		print("lock2: wait")
+		lock2.lock()
+//		print("lock2: waited")
+//		let allPathsUnique = Set(allPaths)
 //		print(allPathsUnique)
 		return "\(allPathsUnique.count)"
 	}
